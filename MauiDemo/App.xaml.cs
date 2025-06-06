b1719d0b48767d786fd32f1a38a8278183e57fda
Goncalo Russo <goncalorusso@gmail.com>
2025-06-06 19:58:02 +0100

    FlexLayout example

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -14,7 +14,7 @@
             navPage.BarBackground = Colors.Yellow;
             navPage.BarTextColor = Colors.Blue;
 
-            return new Window(new GridLayoutDemo());
+            return new Window(new FlexLayoutDemo());
         }
     }
 }
