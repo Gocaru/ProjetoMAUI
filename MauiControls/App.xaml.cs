62351de65e8e562b38bc61a61d8af2a3c7a53644
Goncalo Russo <goncalorusso@gmail.com>
2025-06-07 12:57:15 +0100

    Introduce base structure with controllers and MAUI views

--- MauiControls/App.xaml.cs
+++ MauiControls/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MauiControls
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MauiPresentationControls());
+            return new Window(new MauiCommandControls());
         }
     }
 }
