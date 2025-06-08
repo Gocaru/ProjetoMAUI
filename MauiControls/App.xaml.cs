73b6c03250911dc2e6271fd5db713ad9b9633742
Goncalo Russo <goncalorusso@gmail.com>
2025-06-08 09:47:01 +0100

    Add UI inputs and interaction events (Entry, Button, Progress, etc.)

--- MauiControls/App.xaml.cs
+++ MauiControls/App.xaml.cs
@@ -11,7 +11,7 @@ namespace MauiControls
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MauiSetValueControls());
+            return new Window(new MauiEditControls());
         }
     }
 }
